0594843946aa44b3038b5b8ef279f3602374888b
Shahinurhasans <95462455+Shahinurhasans@users.noreply.github.com>
2026-08-29 22:53:50 +0600

    residual /skip connection added

--- model.py
+++ model.py
@@ -95,3 +95,15 @@ class MultiHeadAttentionBlock(nn.Module):
         x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
         x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k) 
         return self.w_o(x)
+
+
+
+class ResidualConnection(nn.Module):
+    
+        def __init__(self, features: int, dropout: float) -> None:
+            super().__init__()
+            self.dropout = nn.Dropout(dropout)
+            self.norm = LayerNormalization(features)
+    
+        def forward(self, x, sublayer):
+            return x + self.dropout(sublayer(self.norm(x)))
