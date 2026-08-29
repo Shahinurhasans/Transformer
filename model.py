8a41452992acb2855637a03a46e16d7f2079efda
Shahinurhasans <95462455+Shahinurhasans@users.noreply.github.com>
2026-08-29 21:46:51 +0600

    Feed Forward Layer added

--- model.py
+++ model.py
@@ -47,4 +47,12 @@ class LayerNormalization(nn.Module):
     
         
         
-        
+class FeedForward(nn.Module):
+    def __init__(self,d_model:int,d_ff:int,dropout:float)->None:
+        super().__init__()
+        self.linear1=nn.Linear(d_model,d_ff)
+        self.dropout=nn.Dropout(dropout)
+        self.linear2=nn.Linear(d_ff,d_model)
+
+    def forward(self,x):
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
